8f9e9d5f0d3307e930dff1e2ef2f2a56de27e04f
Fido García <garciafido@gmail.com>
2020-05-11 17:18:09 -0300

    blob to netcdf file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# cima-goes-examples
+Installation with Anaconda
+--------------------------
+
+.. code-block:: console
+
+    $ conda create -n cima-goes-examples python=3.8
+    $ conda activate cima-goes-examples
+    $ pip install cima.goes
+
+
+Obtaining GCP Credentials
+-------------------------
+
+https://cloud.google.com/docs/authentication/production#obtaining_and_providing_service_account_credentials_manually
---
+++ blob_to_netcdf_file.py
@@ -0,0 +1,54 @@
+########################
+# Define GOES 16 Product
+########################
+
+from cima.goes import ProductBand, Product, Band
+
+product_band = ProductBand(
+    product=Product.CMIPF,              # Cloud and Moisture Image Product – Full Disk
+    band=Band.CLEAN_LONGWAVE_WINDOW,    # 13
+)
+
+
+######################
+# Define date and time
+######################
+
+year = 2020
+month = 5
+day = 1
+hour = 22
+
+
+########################
+# Get Google Cloud Blobs
+########################
+
+from cima.goes.storage import GCS
+from gcs_credentials import credentials
+
+gcs = GCS(credentials_as_dict=credentials)
+band_blobs = gcs.one_hour_blobs(year=year, month=month, day=day, hour=hour, product_band=product_band)
+
+
+###################
+# Save NETCDF Files
+###################
+
+import netCDF4
+
+print(f'Product: {product_band.product.__doc__}')
+print(f'Band: {product_band.band.__doc__}')
+print(f'Date: {year}-{month}-{day} at {hour} hrs.')
+
+for blob in band_blobs.blobs:
+    data = gcs.download_from_blob(blob)
+    dataset = netCDF4.Dataset("in_memory_file", mode='r', memory=data)
+
+    datetime = dataset.date_created
+    filename = f'Cloud_and_Moisture_{datetime}.nc'
+    with open(filename, 'wb') as f:
+        f.write(data)
+
+    minute = datetime[14:16]
+    print(f'    {hour}:{minute} (saved as {filename})')
